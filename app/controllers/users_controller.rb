7fd887239621d4eb30e2759986fc71099a2322e2
k4ju <kjan79@gmail.com>
2015-08-24 19:05:41 +0000

    Sign-up in progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new()
+  end
+  
+  def index
+  end
+  
+  def update
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #Do save
+    else
+      render 'new'
+    end
+  end
+  
 end
